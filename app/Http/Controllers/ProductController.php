0816c1eedd3b41dcac2462c09ac34b3c9ffe9c87
stopdam <sky_line3@outlook.co.th>
2021-02-02 20:48:10 +0700

    logout and login done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use Session;
+
 
 class ProductController extends Controller
 {
@@ -22,6 +26,27 @@ class ProductController extends Controller
     {
         $data= Product::where('name', 'like','%' .$req->input('query').'%')->get();
         return view('search',['products'=>$data]);
+    }
+    function addToCart(Request $req)
+    {
+        if($req->session()->has('user'))
+        {
+            $cart= new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+        }
+        else
+        {
+            return redirect('/login');
+        }
+       
+    }
+    static function cartItem()
+    {
+        $userId=Session::get('user')['id'] ;
+        return Cart::where('user_id',$userId)->count();
     }
 }
 
